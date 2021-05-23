1ab331b96ca950e8c814c017937c1341944744f7
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-05-23 09:58:32 -0300

    finish

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 //arquivo do adonis utilizamos use
+const Database = use("Database");
 const User = use("App/Models/User");
 
 class UserController {
@@ -8,7 +9,15 @@ class UserController {
     //Quero pegar do usuario quando ele enviar o formulario.
     const data = request.only(["username", "email", "password"]);
 
-    const user = await User.create(data);
+    //Salvar o addresses do user
+    const addresses = request.input("addresses");
+    //Olhar se nao ha error, antes de salvar no banco de dados de
+    const trx = await Database.beginTransaction();
+
+    const user = await User.create(data, trx);
+    await user.addresses().createMany(addresses, trx);
+
+    await trx.commit();
     return user;
   }
 }
